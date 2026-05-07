8c8da63530ec7e7115a0322c78c1c72bcc095a00
Dave Bunyan <davebunyan1@gmail.com>
2026-05-07 11:50:17 -0400

    Added prisma

---
+++ prisma/migrations/20260507154852_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Word" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "characters" TEXT NOT NULL,
+    "pinyin" TEXT NOT NULL,
+    "english" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
